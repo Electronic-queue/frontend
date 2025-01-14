f64ab7416660e835d7210304ae7ca59c97097d46
UmidIsanov <isanovumid8@gmail.com>
2025-01-14 13:19:35 +0500

    feat: added cards

--- src/features/pages/QueuePage.tsx
+++ src/features/pages/QueuePage.tsx
@@ -2,6 +2,9 @@ import { Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import StatusCard from '../../components/StatusCard';
 import CheckMarkIcon from '../../assets/CheckMarkIcon';
+import CancelIcon from '../../assets/CancelIcon';
+import ExclamationMarkIcon from '../../assets/ExclamationMarkIcon';
+import LoadingIcon from '../../assets/LoadingIcon';
 const QueuePage = () => {
   const { t } = useTranslation('translation');
 
@@ -14,7 +17,18 @@ const QueuePage = () => {
     >
       {/* <p>{t('I18N_QUEUE_MANAGEMENT')}</p> */}
 
-      <StatusCard icon={<CheckMarkIcon />} text={t('accepteds')} number={5} />
+      <StatusCard icon={<CheckMarkIcon />} text={t('accepteds')} number={75} />
+      <StatusCard icon={<CancelIcon />} text={t('not_accepteds')} number={3} />
+      <StatusCard
+        icon={<ExclamationMarkIcon />}
+        text={t('redirected')}
+        number={5}
+      />
+      <StatusCard
+        icon={<LoadingIcon />}
+        text={t('in_anticipation')}
+        number={5}
+      />
     </Stack>
   );
 };
