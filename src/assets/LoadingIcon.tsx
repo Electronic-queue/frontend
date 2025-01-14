d7aa57428867addf7d0604c47f258b61c0faefcb
UmidIsanov <isanovumid8@gmail.com>
2025-01-14 12:45:48 +0500

    feat: add status card

---
+++ src/assets/LoadingIcon.tsx
@@ -0,0 +1,15 @@
+const LoadingIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="33"
+      viewBox="0 0 32 33"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M-1.5 -1H33.5V34H-1.5V-1Z" fill="#2E90FA" />
+    </svg>
+  );
+};
+
+export default LoadingIcon;
--- src/features/pages/QueuePage.tsx
+++ src/features/pages/QueuePage.tsx
@@ -1,6 +1,7 @@
 import { Stack } from '@mui/material';
 import { useTranslation } from 'react-i18next';
-
+import StatusCard from '../../components/StatusCard';
+import CheckMarkIcon from '../../assets/CheckMarkIcon';
 const QueuePage = () => {
   const { t } = useTranslation('translation');
 
@@ -11,7 +12,9 @@ const QueuePage = () => {
       justifyContent="center"
       height="100vh"
     >
-      <p>{t('I18N_QUEUE_MANAGEMENT')}</p>
+      {/* <p>{t('I18N_QUEUE_MANAGEMENT')}</p> */}
+
+      <StatusCard icon={<CheckMarkIcon />} text={t('accepteds')} number={5} />
     </Stack>
   );
 };
