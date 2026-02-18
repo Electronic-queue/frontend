ab611b8196d346e21562ed7d153584c59562ef67
UmidIsanov <isanovumid8@gmail.com>
2026-02-18 14:02:18 +0500

    fix: logo

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,7 +3,7 @@ export { ReactComponent as CheckMarkIcon } from "./check-mark-icon.svg";
 export { ReactComponent as ExclamationMarkIcon } from "./exclamation-mark-icon.svg";
 export { ReactComponent as LoadingIcon } from "./loading-icon.svg";
 export { ReactComponent as UserLogo } from "./user-logo-icon.svg";
-export { ReactComponent as SULogo } from "./suLogo.svg";
+export { ReactComponent as SULogo } from "./su-logo.svg";
 export { ReactComponent as SULogoM } from "./su-logo-m.svg";
 export { ReactComponent as SULogoMDark } from "./su-logo-m-dark.svg";
 export { ReactComponent as PhoneIcon } from "./phoneIcon.svg";
